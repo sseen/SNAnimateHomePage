84a74c40a56638130a6f3ebccac72e84d2217abd
sseen <ssnfee@gmail.com>
2016-08-31 10:57:36 +0800

    Initial Commit

---
+++ ContainerVC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContainerVC
+//
+//  Created by sseen on 2016/8/31.
+//  Copyright © 2016年 sseen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ContainerVC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContainerVC
+//
+//  Created by sseen on 2016/8/31.
+//  Copyright © 2016年 sseen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
